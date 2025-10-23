5f0994de4af4ced3f90a7c5e3b0298cfa8140dc7
levikabd <levikabd@yandex.ru>
2025-10-23 10:30:35 +0700

    add root window

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("startxpy")
+root.geometry("250x200")
+
+
+def click():
+    window = Tk()
+    window.title("Новое окно")
+    window.geometry("250x200")
+
+
+button = ttk.Button(text="Создать окно", command=click)
+button.pack(anchor=CENTER, expand=1)
+
+root.mainloop()
